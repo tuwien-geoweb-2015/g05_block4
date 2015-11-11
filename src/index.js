44b4d3ad95228cf3172f7786efdde1137944e41b
sonjal <sonja.list@hotmail.com>
2015-11-11 18:25:00 +0100

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('../node_modules/openlayers/dist/ol.css');
 require('./index.css');
+
 var ol = require('openlayers');
 
 var map = new ol.Map({
@@ -8,15 +9,6 @@ var map = new ol.Map({
   layers: [
     new ol.layer.Tile({
       source: new ol.source.OSM()
-    }),
-    new ol.layer.Tile({
-      source: new ol.source.TileWMS({
-        url: url,
-        params: {LAYERS: prefix + ':' + layer}
-      })
-    }),
-    new ol.layer.Vector({
-      source: feedbackPoints
     })
   ],
   view: new ol.View({
@@ -27,8 +19,8 @@ var map = new ol.Map({
 
 var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
 var layer = 'feedback';
-var prefix = 'g05_2015';
-var featureNS = 'http://g05/2015';
+var prefix = 'ifip_2015'; // Ersetzen durch euren Arbeitsbereich-Namen
+var featureNS = 'http://ifip/2015'; // Ersetzen durch eure Namensraum-URI
 var form = document.getElementById('feedback');
 
 var feature = new ol.Feature();
@@ -41,30 +33,4 @@ send(form, feature, url, {
   featurePrefix: prefix,
   featureNS: featureNS,
   srsName: map.getView().getProjection().getCode()
-});
-
-var feedbackPoints = new ol.source.Vector({
-  features: new ol.Collection(),
-});
-
-map.addLayer(new ol.layer.Vector({ source: feedbackPoints }));
-feedbackPoints.addFeature(feature);
-var modify = new ol.interaction.Modify({
-  features: feedbackPoints.getFeaturesCollection()
-});
-map.addInteraction(modify);
-
-
-var modify = new ol.interaction.Modify({
-  features: feedbackPoints.getFeaturesCollection()
-});
-map.addInteraction(modify);
-
-var geolocation = new ol.Geolocation({
-  projection: map.getView().getProjection(),
-  tracking: true
-});
-geolocation.once('change:position', function(evt) {
-  feature.getGeometry().setCoordinates(geolocation.getPosition());
-  map.getView().setCenter(geolocation.getPosition());
 });
