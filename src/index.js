adf12b0767f5c2cc2a9d4557673151238c58aa84
sonjal <sonja.list@hotmail.com>
2015-11-11 18:10:58 +0100

    Exercise 1 - Head

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ require('../node_modules/openlayers/dist/ol.css');
 require('./index.css');
 var ol = require('openlayers');
 
-
-var feedbackPoints = new ol.source.Vector({
-  features: new ol.Collection(),
-});
-
 var map = new ol.Map({
   controls: ol.control.defaults({attributionOptions: {collapsible: false}}),
   target: 'map',
@@ -30,7 +25,36 @@ var map = new ol.Map({
   })
 });
 
+var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
+var layer = 'feedback';
+var prefix = 'g05_2015';
+var featureNS = 'http://g05/2015';
+var form = document.getElementById('feedback');
+
+var feature = new ol.Feature();
+feature.setGeometryName('geom');
+feature.setGeometry(new ol.geom.Point(map.getView().getCenter()));
+
+var send = require('./feedback_send');
+send(form, feature, url, {
+  featureType: layer,
+  featurePrefix: prefix,
+  featureNS: featureNS,
+  srsName: map.getView().getProjection().getCode()
+});
+
+var feedbackPoints = new ol.source.Vector({
+  features: new ol.Collection(),
+});
+
+map.addLayer(new ol.layer.Vector({ source: feedbackPoints }));
 feedbackPoints.addFeature(feature);
+var modify = new ol.interaction.Modify({
+  features: feedbackPoints.getFeaturesCollection()
+});
+map.addInteraction(modify);
+
+
 var modify = new ol.interaction.Modify({
   features: feedbackPoints.getFeaturesCollection()
 });
@@ -43,23 +67,4 @@ var geolocation = new ol.Geolocation({
 geolocation.once('change:position', function(evt) {
   feature.getGeometry().setCoordinates(geolocation.getPosition());
   map.getView().setCenter(geolocation.getPosition());
-});
-
-var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
-var layer = 'feedback';
-var prefix = 'g05_2015';
-var featureNS = 'http://g05/2015';
-var form = document.getElementById('feedback');
-
-
-var feature = new ol.Feature();
-feature.setGeometryName('geom');
-feature.setGeometry(new ol.geom.Point(map.getView().getCenter()));
-
-var send = require('./feedback_send');
-send(form, feature, url, {
-  featureType: layer,
-  featurePrefix: prefix,
-  featureNS: featureNS,
-  srsName: map.getView().getProjection().getCode()
 });
