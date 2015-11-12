5689bb26e37bd91fc04c8328e8be128ce14c5689
sonjal <sonja.list@hotmail.com>
2015-11-12 10:14:49 +0100

    index.js Locationpoints

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 require('../node_modules/openlayers/dist/ol.css');
 require('./index.css');
-
 var ol = require('openlayers');
+var send = require('./feedback_send');
+
+var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
+var layer = 'feedback';
+var prefix = 'ifip_2015';
+var featureNS = 'http://ifip/2015';
+var form = document.getElementById('feedback');
+
+var feedbackPoints = new ol.source.Vector({
+  features: new ol.Collection(),
+});
 
 var map = new ol.Map({
   controls: ol.control.defaults({attributionOptions: {collapsible: false}}),
@@ -9,6 +19,15 @@ var map = new ol.Map({
   layers: [
     new ol.layer.Tile({
       source: new ol.source.OSM()
+    }),
+    new ol.layer.Tile({
+      source: new ol.source.TileWMS({
+        url: url,
+        params: {LAYERS: prefix + ':' + layer}
+      })
+    }),
+    new ol.layer.Vector({
+      source: feedbackPoints
     })
   ],
   view: new ol.View({
@@ -17,17 +36,24 @@ var map = new ol.Map({
   })
 });
 
-var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
-var layer = 'feedback';
-var prefix = 'g05_2015'; // Ersetzen durch euren Arbeitsbereich-Namen
-var featureNS = 'http://g05/2015'; // Ersetzen durch eure Namensraum-URI
-var form = document.getElementById('feedback');
-
 var feature = new ol.Feature();
 feature.setGeometryName('geom');
 feature.setGeometry(new ol.geom.Point(map.getView().getCenter()));
+feedbackPoints.addFeature(feature);
+var modify = new ol.interaction.Modify({
+  features: feedbackPoints.getFeaturesCollection()
+});
+map.addInteraction(modify);
 
-var send = require('./feedback_send');
+var geolocation = new ol.Geolocation({
+  projection: map.getView().getProjection(),
+  tracking: true
+});
+geolocation.once('change:position', function(evt) {
+  feature.getGeometry().setCoordinates(geolocation.getPosition());
+  map.getView().setCenter(geolocation.getPosition());
+});
+
 send(form, feature, url, {
   featureType: layer,
   featurePrefix: prefix,
